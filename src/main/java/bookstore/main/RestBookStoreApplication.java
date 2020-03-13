bdbe4e61cc4316be939b9fa0b91b7ebf4a0131d7
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-03-14 10:54:20 +1100

    First commit

---
+++ src/main/java/bookstore/main/RestBookStoreApplication.java
@@ -0,0 +1,13 @@
+package bookstore.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBookStoreApplication.class, args);
+	}
+
+}
