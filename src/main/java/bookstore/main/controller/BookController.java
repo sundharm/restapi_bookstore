e18eb75a91498d29ac6504e05d45a58729faa9cd
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-03-15 10:12:40 +1100

    Added delete book and add books endpoints

--- src/main/java/bookstore/main/controller/BookController.java
+++ src/main/java/bookstore/main/controller/BookController.java
@@ -1,12 +1,15 @@
 package bookstore.main.controller;
 
+import bookstore.main.exception.NotFoundException;
 import bookstore.main.model.Book;
 import bookstore.main.repos.BookRepository;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +32,20 @@ public class BookController {
 	public List<Book> getBooks() {
 		return bookRepo.findAll();
 	}
+	
+	 @PostMapping("/books")
+	 public String addBooks(@RequestBody List<Book> bookList){
+	    bookRepo.saveAll(bookList);
+	    return "Books saved successfully..";
+	 }
+	
+	@DeleteMapping("/book/{id}")
+    public String deleteBook(@PathVariable int id) {
+        return bookRepo.findById(id)
+                .map(book -> {
+                    bookRepo.delete(book);
+                    return "Deleted Successfully!";
+                }).orElseThrow(() -> new NotFoundException("Book not found with the given id " + id));
+    }
 
 }
