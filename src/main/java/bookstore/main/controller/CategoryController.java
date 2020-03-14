f6c48a63f66b5e64fce9db33e20a8b0af37cd472
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-03-14 15:45:31 +1100

    Added post category and exception

--- src/main/java/bookstore/main/controller/CategoryController.java
+++ src/main/java/bookstore/main/controller/CategoryController.java
@@ -1,5 +1,37 @@
 package bookstore.main.controller;
 
+import bookstore.main.model.Category;
+import bookstore.main.repos.CategoryRepository;
+import bookstore.main.repos.BookRepository;
+import bookstore.main.exception.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
+	
+	@Autowired
+	private CategoryRepository categoryRepo;
+	
+	@Autowired
+	private BookRepository bookRepo;
+	
+	@PostMapping("/book/{book_id}/category")
+	public Category addCategory(@PathVariable int book_id, @RequestBody Category category) {
+		return bookRepo.findById(book_id).map(book->{
+				category.setBook(book);
+				return categoryRepo.save(category);
+				}).orElseThrow(() -> new NotFoundException("Book not found!"));
+	}
+	
+	
+	
 
 }
---
+++ src/main/java/bookstore/main/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package bookstore.main.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "NotFoundException not found")
+public class NotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
