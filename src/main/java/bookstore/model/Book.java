ec2acb64c76385a65d5cf50f08086f473d01b69f
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-03-14 11:49:31 +1100

    Added entities to book and category

--- src/main/java/bookstore/model/Book.java
+++ src/main/java/bookstore/model/Book.java
@@ -1,5 +1,53 @@
 package bookstore.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="books")
 public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="book_name")
+	private String bookName;
+	
+	@Column(name="author_name")
+	private String authorName;
+	
+	public Book() {
+		
+	}
+	
+	public Book(int id, String bookName, String authorName) {
+		this.id = id;
+		this.bookName = bookName;
+		this.authorName = authorName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
 
 }
--- src/main/java/bookstore/model/Category.java
+++ src/main/java/bookstore/model/Category.java
@@ -1,5 +1,37 @@
 package bookstore.model;
 
-public class Category {
+import javax.persistence.*;
 
+@Entity
+@Table(name="categories")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "category_name")
+	private String categoryName;
+
+	public Category(int id, String categoryName) {
+		this.id = id;
+		this.categoryName = categoryName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
 }
