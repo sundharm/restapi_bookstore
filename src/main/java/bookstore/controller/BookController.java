cf7db2f0faff50c8009219b1202f1703aae1d897
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-03-14 12:45:58 +1100

    Added endpoint to add book and get all books

--- src/main/java/bookstore/controller/BookController.java
+++ src/main/java/bookstore/controller/BookController.java
@@ -1,5 +1,33 @@
 package bookstore.controller;
 
+import bookstore.model.Book;
+import bookstore.repos.BookRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api")
 public class BookController {
+	
+	@Autowired
+	private BookRepository bookRepo;
+	
+	@PostMapping("/book")
+	public Book addBook(@RequestBody Book book) {
+		return bookRepo.save(book);
+	}
+	
+	@GetMapping("/book")
+	public List<Book> getBooks() {
+		return bookRepo.findAll();
+	}
 
 }
