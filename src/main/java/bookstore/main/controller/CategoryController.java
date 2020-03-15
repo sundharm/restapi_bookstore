2226039acfe7c76f2ea8dbbe904c395013216648
Sundharamurthy <s3700565@student.rmit.edu.au>
2020-03-15 11:23:10 +1100

    Added delete category endpoint

--- src/main/java/bookstore/main/controller/CategoryController.java
+++ src/main/java/bookstore/main/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package bookstore.main.controller;
 
+import bookstore.main.model.Book;
 import bookstore.main.model.Category;
 import bookstore.main.repos.CategoryRepository;
 import bookstore.main.repos.BookRepository;
@@ -8,6 +9,7 @@ import bookstore.main.exception.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,10 +30,22 @@ public class CategoryController {
 		return bookRepo.findById(book_id).map(book->{
 				category.setBook(book);
 				return categoryRepo.save(category);
-				}).orElseThrow(() -> new NotFoundException("Book not found!"));
+				}).orElseThrow(() -> new NotFoundException("Book not found"));
+	
 	}
 	
-	
-	
-
+	@DeleteMapping("/book/{book_id}/category/{category_id}")
+    public String deleteCategory(@PathVariable int book_id,
+                                   @PathVariable int category_id) {
+		
+        if(!bookRepo.existsById(book_id)) {
+            throw new NotFoundException("Book not found!");
+        }       
+        return categoryRepo.findById(category_id)
+                .map(category -> {
+                    categoryRepo.delete(category);
+                    return "Deleted Successfully";
+                }).orElseThrow(() -> new NotFoundException("Category not found"));
+    }
 }
+	
